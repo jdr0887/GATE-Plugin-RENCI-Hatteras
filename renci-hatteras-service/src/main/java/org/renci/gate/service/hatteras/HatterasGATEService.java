30c82b6e15db74a70934e5c7dbc002e0f90798a9
Jason Reilly <jdr0887@gmail.com>
2016-03-01 15:42:48 -0500

    adding hostAllow

--- renci-hatteras-service/src/main/java/org/renci/gate/service/hatteras/HatterasGATEService.java
+++ renci-hatteras-service/src/main/java/org/renci/gate/service/hatteras/HatterasGATEService.java
@@ -20,7 +20,6 @@ import org.renci.jlrm.slurm.SLURMJobStatusInfo;
 import org.renci.jlrm.slurm.SLURMJobStatusType;
 import org.renci.jlrm.slurm.ssh.SLURMSSHKillCallable;
 import org.renci.jlrm.slurm.ssh.SLURMSSHLookupStatusCallable;
-import org.renci.jlrm.slurm.ssh.SLURMSSHSubmitCondorGlideinCallable;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -107,8 +106,7 @@ public class HatterasGATEService extends AbstractGATEService {
         try {
             logger.info("siteInfo: {}", getSite());
             logger.info("queueInfo: {}", queue);
-            String hostAllow = "*.unc.edu";
-            SLURMSSHSubmitCondorGlideinCallable callable = new SLURMSSHSubmitCondorGlideinCallable();
+            HatterasSubmitCondorGlideinCallable callable = new HatterasSubmitCondorGlideinCallable();
             callable.setCollectorHost(getCollectorHost());
             callable.setUsername(System.getProperty("user.name"));
             callable.setSite(getSite());
@@ -116,8 +114,8 @@ public class HatterasGATEService extends AbstractGATEService {
             callable.setQueue(queue);
             callable.setSubmitDir(submitDir);
             callable.setRequiredMemory(40);
-            callable.setHostAllowRead(hostAllow);
-            callable.setHostAllowWrite(hostAllow);
+            callable.setHostAllowRead(getHostAllow());
+            callable.setHostAllowWrite(getHostAllow());
             Executors.newSingleThreadExecutor().submit(callable).get();
         } catch (Exception e) {
             throw new GATEException(e);
